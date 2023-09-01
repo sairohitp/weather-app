d16c36c9dd0c3462fdce9ef661530effb1253e7e
sairohitp <sairohitp007@gmail.com>
2023-09-01 22:18:53 +0530

    js:

--- index.js
+++ index.js
@@ -1,11 +1,19 @@
 const apikey = "e4d2344c2546181de41edeaf454a1304";
-const apiURL = "https://api.openweathermap.org/data/2.5/weather?q=germany&units=metric";
+const apiURL = "https://api.openweathermap.org/data/2.5/weather?units=metric&q=";
 
-async function checkWeather() {
-    const response = await fetch(apiURL + `&appid=${apikey}`);
+const searchBox = document.querySelector(".search");
+
+async function checkWeather(city) {
+    const response = await fetch(apiURL + city + `&appid=${apikey}`);
     var data = await response.json();
 
     console.log(data)
+
+    document.querySelector(".city").innerHTML = data.name
+    document.querySelector(".temp").innerHTML = Math.round(data.main.temp) + "°C"
+    document.querySelector(".humidity").innerHTML = data.main.humidity + "%"
+    document.querySelector(".wind").innerHTML = data.wind.speed + " km/h"
 }
 
 checkWeather()
+
